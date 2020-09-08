2b5cbd8ac41870a734ed5facef6f069c0bd7f831
Mauro Marino <macbook@MacBook-Pro.local>
2020-09-08 19:34:57 -0300

    first commit

---
+++ src/main/java/ar/edu/unq/desapp/grupoK/backenddesappapi/BackendDesappApiApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoK.backenddesappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDesappApiApplication.class, args);
+	}
+
+}
