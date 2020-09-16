104e164a327885deee8592965de8344e6d8eaae5
Mauro Marino <macbook@MacBook-Pro.local>
2020-09-15 21:49:51 -0300

    implementation proyect

--- src/main/java/ar/edu/unq/desapp/grupoK/backenddesappapi/model/Proyect.java
+++ src/main/java/ar/edu/unq/desapp/grupoK/backenddesappapi/model/Proyect.java
@@ -7,19 +7,43 @@ public class Proyect {
     Location location;
     String dateEnd ;
     String dateStart;
-    Integer percentage;
-    Integer factorStart;
-    Integer factorEnd;
+    Integer percentage = 100;
+    Integer factor = 1000;
+    Integer moneyNeededForProject = 0;
+    Integer moneyReceiveForProject = 0;
 
-    Proyect(String name , ArrayList<User> donor, Location location , String dateEnd ,String dateStart ,Integer percentage ,Integer factorStart,Integer factorEnd){
+    Proyect(String name , ArrayList<User> donor, Location location , String dateEnd ,String dateStart ,Integer percentage ,Integer factor ){
             this.proyectName = name;
             this.donor = donor;
             this.location = location;
             this.dateEnd = dateEnd;
             this.dateStart = dateStart;
             this.percentage = percentage;
-            this.factorStart = factorStart;
-            this.factorEnd = factorEnd;
+            this.factor = factor;
     }
-    
+
+    private Integer moneyNeededForProject (){
+        return moneyNeededForProject = location.population * factor;
+    }
+
+    private void setFactor (Integer newFactor){
+        factor = newFactor;
+    }
+
+    public void receiveDonation(Integer money, User user) {
+        moneyReceiveForProject += money;
+        donor.add(user);
+        sendPointDonator(money, user);
+    }
+
+    private void sendPointDonator(Integer money, User user) {
+        Integer points = money;
+        if ( money < 1000) {
+            points = 200;
+        }
+        if ( location.population < 2000){
+            points = points * 2;
+        }
+        user.giveMePoints(points);
+    }
 }
--- src/main/java/ar/edu/unq/desapp/grupoK/backenddesappapi/model/User.java
+++ src/main/java/ar/edu/unq/desapp/grupoK/backenddesappapi/model/User.java
@@ -1,5 +1,8 @@
 package ar.edu.unq.desapp.grupoK.backenddesappapi.model;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class User {
     String name;
     String pass;
@@ -7,6 +10,9 @@ public class User {
     Integer points;
     String email;
     Integer donatedMoney;
+    Boolean flagDonate = false;
+    int month;
+    ArrayList<Proyect> donatedProyects;
 
     User(String name,
          String pass,
@@ -14,14 +20,33 @@ public class User {
          Integer points,
          String email,
          Integer donatedMoney){
-        
+
             this.name = name;
             this.pass = pass;
             this.nick = nick;
             this.points = points;
             this.email = email;
             this.donatedMoney = donatedMoney;
+            this.month = Calendar.MONTH;
 
     }
 
+    private void donate (Proyect proyect, Integer money){
+        this.donatedProyects.add(proyect);
+        proyect.receiveDonation(money, this);
+        this.donatedMoney += money;
+        validateFlagDonate();
+    }
+
+    public void giveMePoints(Integer points) {
+        this.points += points;
+    }
+
+    private void validateFlagDonate (){
+        if( Calendar.MONTH != this.month){
+            this.flagDonate = false;
+        }else {
+            this.flagDonate = true;
+        }
+    }
 }
